f644305311f07223712c2cb19867c9ad89a00cbe
18parnitae <121769080+18parnitae@users.noreply.github.com>
2023-03-01 20:47:03 +0000

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -4,9 +4,9 @@ function addUser() {
   player2_name = document.getElementById("player2_name_input").value;
 
     //Set "player1_name" using localStorage.setItem() function
-    localStorage.setItem("player1_name"); 
-    
 	  //Set "player2_name" using localStorage.setItem() function
+    localStorage.setItem("player1_name")
+    localStorage.setItem("player2_name")
 
     window.location = "game_page.html";
 }
